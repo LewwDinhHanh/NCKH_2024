887c85ce6208aa5746852c1ea9eb945943e0623c
LewwDinhHanh <hanhkk0909@gmail.com>
2024-05-10 18:18:47 +0700

    update

---
+++ function/dictionary.js
@@ -0,0 +1,11 @@
+show = document.getElementById("show");
+
+function showmore(){
+
+    show.style.display = "block";
+    parent.up();
+}
+
+function closeShow(){
+        show.style.display = "none";
+}
